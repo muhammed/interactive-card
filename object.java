bbe461b64e6647b145ee5479b8aee724b8f520f9
Mohamad Nurul Ihsan <44545824+ikhsanlosiver@users.noreply.github.com>
2019-10-29 21:00:04 +0800

    Create object.java

---
+++ object.java
@@ -0,0 +1,24 @@
+
+
+public class Jobsheet1 {
+
+    void Mobil(){
+        System.out.println("Avanza");
+    }
+
+    void harga(){
+        System.out.println("120 juta");
+    }
+
+    void tahun(){
+        System.out.println("2014");
+    }
+
+    public static void main(String[] args) {
+Jobsheet1 ob = new Jobsheet1();
+        ob.Mobil();
+        ob.harga();
+        ob.tahun();
+    }
+
+}
