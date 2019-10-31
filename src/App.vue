2241dcdd44b4052c195edfb037bc7727e0a79f18
Muhammed Erdem <iamuhammederdem@gmail.com>
2019-10-31 09:23:21 +0300

    add input events

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div class="wrapper" id="app">
     <CardForm
       :form-data="formData"
+      @input-card-number="updateCardNumber"
+      @input-card-name="updateCardName"
+      @input-card-month="updateCardMonth"
+      @input-card-year="updateCardYear"
+      @input-card-cvv="updateCardCvv"
     />
     <!-- backgroundImage="https://images.unsplash.com/photo-1572336183013-960c3e1a0b54?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" -->
   </div>
